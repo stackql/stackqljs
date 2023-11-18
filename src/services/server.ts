19b09132f6fba97f2d598df901ec9312978956fe
yunchengyang <fabioyang781@gmail.com>
2023-11-18 18:33:55 +1100

    added retries

--- src/services/server.ts
+++ src/services/server.ts
@@ -8,21 +8,36 @@ export class Server {
   constructor() {}
 
   public async connect(connectionString?: string) {
-    try {
-      // Good practice is to get the connection URI from an environment variable
-      const connection = Deno.env.get("POSTGRES") || connectionString;
+    const maxRetries = 3;
+    let currentAttempt = 0;
 
-      if (!connection) {
-        throw new Error(
-          "Connection string not found \n Please set the POSTGRES environment variable or pass the connection string as an argument",
-        );
+    while (currentAttempt < maxRetries) {
+      try {
+        // Attempt to connect
+        const connection = Deno.env.get("POSTGRES") || connectionString;
+        if (!connection) {
+          throw new Error(
+            "Connection string not found \n Please set the POSTGRES environment variable or pass the connection string as an argument",
+          );
+        }
+
+        console.log("connecting", connection);
+        this.pg = await pgconnect(connection);
+        console.log("connected");
+        return this.pg;
+      } catch (error) {
+        currentAttempt++;
+        console.log(`Attempt ${currentAttempt} failed: ${error.message}`);
+
+        if (currentAttempt >= maxRetries) {
+          throw new Error(
+            `Could not connect to the server after ${maxRetries} attempts: ${error.message}`,
+          );
+        }
+
+        // Wait for 1 second before the next attempt
+        await new Promise((resolve) => setTimeout(resolve, 1000));
       }
-      console.log("connecting", connection);
-      this.pg = await pgconnect(connection);
-      console.log("connected");
-      return this.pg;
-    } catch (error) {
-      throw new Error(`Could not connect to the server: ${error.message}`);
     }
   }
 
