a82d3335c7ef818d4690d03b6d22115d84c4bc1f
Yuncheng Yang <fabioyang781@gmail.com>
2023-12-19 17:54:58 +1100

    added the build script for npm package

---
+++ scripts/npm-build.ts
@@ -0,0 +1,36 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "https://deno.land/x/dnt/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+typeCheck: false,
+test: false,
+declaration: false,
+scriptModule: false,
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "stackqljs",
+    version: Deno.args[0],
+    description: "StackQL client library for Deno and Node.js that exposes all features StackQL.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/stackql/stackqljs.git",
+    },
+    bugs: {
+      url: "https://github.com/stackql/stackqljs/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
